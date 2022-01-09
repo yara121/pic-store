0190b80919b1a94aa061a42bd825645d21331eae
yara <yaraeb121@gmail.com>
2022-01-09 17:44:34 +0200

    store created

---
+++ src/pages/Photos.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { getClass } from "../utils";
+import Image from "../components/Image";
+import { Context } from "../Context";
+
+function Photos() {
+  const { allPhotos } = useContext(Context);
+  const imageElements = allPhotos.map((img, i) => (
+    <Image key={img.id} img={img} className={getClass(i)} />
+  ));
+  return (
+    <main className="photos">
+ {imageElements}
+    </main>
+  );
+}
+
+export default Photos;
